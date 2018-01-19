35cfd6a5ca4278d57807d4a8f5619696c629c00b
Lexscite <lexscite@gmail.com>
2018-01-19 11:19:25 +0300

    app code repeating fixed

--- client/src/base/app.cpp
+++ client/src/base/app.cpp
@@ -191,8 +191,6 @@ namespace Nixie
 			((MINMAXINFO*)l_param)->ptMinTrackSize.y = 200;
 			return 0;
 		case WM_ACTIVATEAPP:
-			DirectX::Keyboard::ProcessMessage(message, w_param, l_param);
-			return 0;
 		case WM_KEYDOWN:
 		case WM_SYSKEYDOWN:
 		case WM_KEYUP:
